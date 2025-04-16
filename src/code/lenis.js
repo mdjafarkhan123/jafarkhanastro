0d5b32478af1e0437de1f20ea9bfab25a45af08d
Jafar Khan <profile.mdjafarkhan@gmail.com>
2025-04-16 06:30:01 +0600

    Text reveal animtion added

--- src/code/lenis.js
+++ src/code/lenis.js
@@ -1,8 +1,5 @@
 import Lenis from "lenis";
-// Initialize Lenis
 const lenis = new Lenis();
-
-// Use requestAnimationFrame to continuously update the scroll
 function raf(time) {
     lenis.raf(time);
     requestAnimationFrame(raf);
--- src/code/logoslider.js
+++ src/code/logoslider.js
@@ -1,20 +1,56 @@
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import SplitType from "split-type";
 import Swiper from "swiper";
 import { Autoplay } from "swiper/modules";
 document.addEventListener("DOMContentLoaded", () => {
-    new Swiper(".swiper", {
-        modules: [Autoplay],
-        loop: true,
-        slidesPerView: "auto",
-        spaceBetween: 40,
-        centeredSlides: true,
-        autoplay: {
-            delay: 0,
-            disableOnInteraction: false,
-            pauseOnMouseEnter: false,
-            reverseDirection: true, // Also needed for RTL autoplay
-        },
-        speed: 5000,
-        allowTouchMove: false,
-        freeMode: true,
-    });
+    function slider() {
+        new Swiper(".swiper", {
+            modules: [Autoplay],
+            loop: true,
+            slidesPerView: "auto",
+            spaceBetween: 40,
+            centeredSlides: true,
+            autoplay: {
+                delay: 0,
+                disableOnInteraction: false,
+                pauseOnMouseEnter: false,
+                reverseDirection: true,
+            },
+            speed: 5000,
+            allowTouchMove: false,
+            freeMode: true,
+        });
+    }
+
+    slider();
+
+    function textanimmation() {
+        gsap.registerPlugin(ScrollTrigger);
+        document.querySelectorAll(".split-word").forEach((element) => {
+            const text = new SplitType(element, { types: "words" });
+            const scrollConfig = {
+                trigger: element,
+                start: "top 80%",
+                end: "top 20%",
+                scrub: true,
+                toggleActions: "play play reverse reverse",
+            };
+
+            gsap.fromTo(
+                element.querySelectorAll("div.word"),
+                { opacity: 0.3 },
+
+                {
+                    opacity: 1,
+                    duration: 0.3,
+
+                    stagger: 0.02,
+                    scrollTrigger: scrollConfig,
+                }
+            );
+        });
+    }
+
+    textanimmation();
 });
